c74e11cdec677c94f87d11be6ffc609a7f6cbb9c
zhouzhi <1175974811@qq.com>
2021-02-28 19:02:26 +0800

    Jwt 工具类编写

---
+++ yeb_server/src/main/java/com/happy/server/config/security/JwtTokenUtil.java
@@ -0,0 +1,150 @@
+package com.happy.server.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Happy
+ * @Date: 2021/02/28/16:17
+ * @Description: Jwt token 工具类
+ */
+@Component
+public class JwtTokenUtil {
+
+    private static final String CLAIM_KEY_USERNAME = "sub";
+    private static final String CLAIM_KEY_CREATED = "created";
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    /**
+     * 根据负载生成 JWT Token
+     * @param claims
+     * @return
+     */
+    private String generateToken(Map<String,Object> claims){
+        return Jwts.builder().setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES512,secret)
+                .compact();
+    }
+
+    /**
+     * 从 token 中获取 JWT 负载
+     * @param token
+     * @return
+     */
+    private Claims getClaimsFromToken(String token){
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJwt(token)
+                    .getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return claims;
+    }
+
+    /**
+     * 生成 token 的过期时间
+     * @return
+     */
+    private Date generateExpirationDate(){
+        return new Date(System.currentTimeMillis() + expiration * 1000);
+    }
+
+    /**
+     * 从 token 中获取过期时间
+     * @param token
+     * @return
+     */
+    private Date getExpiredDateFromToken(String token){
+        Claims claimsFromToken = getClaimsFromToken(token);
+        return claimsFromToken.getExpiration();
+    }
+
+    /**
+     * 判断 token 是否失效
+     * @param token
+     * @return
+     */
+    private boolean isTokenExpired(String token){
+        Date expiredDateFromToken = getExpiredDateFromToken(token);
+        return expiredDateFromToken.before(new Date());
+    }
+
+    /**
+     * 从 token 中获取登录用户名
+     * @param token
+     * @return
+     */
+    public String getUserNameFromToken(String token){
+        String username;
+        try {
+            Claims claimsFromToken = getClaimsFromToken(token);
+            username = claimsFromToken.getSubject();
+        }catch (Exception e){
+            username = null;
+        }
+        return username;
+    }
+
+    /**
+     * 验证 token 是否有效
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public boolean validateToken(String token, UserDetails userDetails){
+        String username = getUserNameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    /**
+     * 根据用户信息生成 token
+     * @param userDetails
+     * @return
+     */
+    public String generateToken(UserDetails userDetails){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put(CLAIM_KEY_USERNAME,userDetails.getUsername());
+        claims.put(CLAIM_KEY_CREATED,new Date());
+        return generateToken(claims);
+    }
+
+    /**
+     * 判断 token 是否可以被刷新
+     * @param token
+     * @return
+     */
+    public boolean canRefresh(String token){
+        return !isTokenExpired(token);
+    }
+
+    /**
+     * 刷新 token
+     * @param token
+     * @return
+     */
+    public String refreshToken(String token){
+        Claims claimsFromToken = getClaimsFromToken(token);
+        claimsFromToken.put(CLAIM_KEY_CREATED,new Date());
+        return generateToken(claimsFromToken);
+    }
+
+}
