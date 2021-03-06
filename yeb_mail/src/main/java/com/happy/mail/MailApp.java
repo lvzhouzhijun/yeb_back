fdf46fed25a8681c0a509d23ddbb4af25554063d
zhouzhi <1175974811@qq.com>
2021-03-06 13:59:23 +0800

    创建发送邮件服务，配置邮件、邮件模板、rabbitmq

---
+++ yeb_mail/src/main/java/com/happy/mail/MailApp.java
@@ -0,0 +1,20 @@
+package com.happy.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Happy
+ * @Date: 2021/03/06/13:53
+ * @Description:
+ */
+@SpringBootApplication
+public class MailApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailApp.class,args);
+    }
+
+}
